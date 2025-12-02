63791f4924e62c30ca9bc0a1decc747f7ff04ae6
Simon Daguet-Kargl <144481209+simdgtk@users.noreply.github.com>
2025-12-02 18:39:19 +0100

    search wip

--- resources/js/components/sections/Navbar.vue
+++ resources/js/components/sections/Navbar.vue
@@ -1,18 +1,24 @@
 <template>
     <nav class="navbar">
-        <a href="/" class="logo-container" @click.prevent="triggerAnimation">
-             <LogoAnimated ref="logoRef" class="logo" />
-        </a>
+                     <a href="/" class="logo-container" @click.prevent="triggerAnimation">
+                             <LogoAnimated
+                                 ref="logoRef"
+                                 class="logo"
+                                 :color1="logoColor1"
+                                 :color2="logoColor2"
+                             />
+                     </a>
     </nav>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import Burger from '../icons/Burger.vue';
-import Logo from '../icons/Logo.vue';
+import { computed, ref } from 'vue';
 import LogoAnimated from '../icons/LogoAnimated.vue';
-
 const logoRef = ref<InstanceType<typeof LogoAnimated> | null>(null);
+
+const currentPath = typeof window !== 'undefined' ? window.location.pathname : '/';
+const logoColor1 = computed(() => currentPath === '/search' ? '#4F49FD' : '#FD494A');
+const logoColor2 = computed(() => '#B8FF93');
 
 const triggerAnimation = () => {
     if (logoRef.value) {
